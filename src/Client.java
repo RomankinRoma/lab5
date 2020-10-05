c443e4f62c366022a1682dd41fd0cc097798b304
Admin <raxo00012345@gmail.com>
2020-10-05 13:49:03 +0600

    ADD

--- src/Client.java
+++ src/Client.java
@@ -6,10 +6,15 @@ import Manufacturer.*;
 public class Client {
     public static void main(String[] args) {
         Car sedan=new Sedan(new Europe(),512.0,"Audi");
-        Car miniven=new Miniven(new US(),512.0,"Audi");
+        Car miniven=new Miniven(new US(),512.0,"BMW");
+        Car lSportCar=new Miniven(new Europe(),512.0,"Porche");
+        Car lSportCar2=new Miniven(new US(),512.0,"Lamborgini");
         Adapter adapterMiniven = new Adapter(miniven);
-        Adapter adapterSedan = new Adapter(sedan);
-        System.out.println(adapterSedan.getSpeed());
+        Adapter adapterLSCar = new Adapter(lSportCar);
+        Adapter adapterLSCar2 = new Adapter(lSportCar2);
+        System.out.println(sedan.getSpeed());
         System.out.println(adapterMiniven.getSpeed());
+        System.out.println(adapterLSCar.getSpeed());
+        System.out.println(adapterLSCar2.getSpeed());
     }
 }
